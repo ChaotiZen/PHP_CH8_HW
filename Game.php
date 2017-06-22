6363d4ec6057f79579d810769f31e90c3f13ee79
Trendon Ellis <trendon.ellis@icloud.com>
2017-06-21 23:02:59 -0500

    Initial commit

---
+++ Game.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simplified Craps</title>
+    <link rel="stylesheet" type="text/css" href="main.css"/>
+</head>
+
+<body>
+<main>
+    <h1>Simplified Craps</h1>
+
+    <div id="rules">
+
+        <p>Rules:</p>
+        <p>After placing your bet in the field below, clicking 'play game' will simulate the rolling of two dice. Based on the sum of those dice, your winnings (or losses) are explained below.</p>
+        <Table>
+            <tr>
+                <th>Sum Of Dice</th>
+                <th>Payoff</th>
+            </tr>
+            <tr>
+                <th>2</th>
+                <th>3:1</th>
+            </tr>
+            <tr>
+                <th>3 or 4</th>
+                <th>1:1</th>
+            </tr>
+            <tr>
+                <th>5 through 9</th>
+                <th>You Lose</th>
+            </tr>
+            <tr>
+                <th>10 or 11</th>
+                <th>2:1</th>
+            </tr>
+            <tr>
+                <th>12</th>
+                <th>5:1</th>
+            </tr>
+
+        </Table>
+    </div>
+
+
+    <h2>Ready to play??</h2>
+    <form action="index.php" method="get">
+        <input type="hidden" name="action" value="process_game">
+
+        <label>User bet:</label>
+        <input type="text" name="userBet"
+               value="<?php echo htmlspecialchars($userBet); ?>"><br>
+
+        <label></label>
+        <?php echo $userBetError ?>
+        <br>
+
+        <label>&nbsp;</label>
+        <input type="submit" value="Play game"><br>
+
+        <label>Number Rolled Die 1:</label>
+        <span><?php echo $die1; ?></span><br>
+        
+        <label>Number Rolled Die 2:</label>
+        <span><?php echo $die2; ?></span><br>
+
+        <label>Die Sum:</label>
+        <span><?php echo $dieSum; ?></span><br>
+        
+        <label>You won:</label>
+        <span><?php echo $AmountWon; ?></span><br>
+
+    </form>
+
+</main>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+
+//set default values to be used when page first loads
+$userBet = 0;
+$die1 = 0;
+$die2 = 0;
+$dieSum = 0;
+
+$userBetError = "";
+$AmountWon = 0.0;
+
+//take action based on variable in POST array
+$action = filter_input(INPUT_GET, 'action');
+
+switch ($action)
+{
+    case 'process_game':
+        $userBet = filter_input(INPUT_GET, 'userBet',
+                FILTER_VALIDATE_FLOAT);
+
+        if (!is_numeric($userBet)||($userBet <= 0))
+        {
+         $userBetError = "You must enter in a numeric value greater than 0...";   
+        }
+
+        $die1 = ($userBetError != "")? 0 : mt_rand(1, 6);
+        $die2 = ($userBetError != "")? 0 : mt_rand(1, 6);
+        $dieSum = $die1 + $die2;
+        $AmountWon = 0.0;
+
+
+        if ($userBetError == "") // no error 
+        {
+            switch ($dieSum)
+            {
+                case 2:
+                    $AmountWon = $userBet * 3;
+                    break;
+                case 3:
+                case 4:
+                    break;
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                    $AmountWon = $userBet - ($userBet * 2);
+                    break;
+                case 10:
+                case 11:
+                    $AmountWon = $userBet * 2;
+                    break;
+                case 12:
+                    $AmountWon = $userBet * 5;
+                    break;
+            }
+        }
+        break;
+}
+include 'Game.php';
+
+?>
